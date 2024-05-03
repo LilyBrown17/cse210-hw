4b56dfb4e0326c7fa0f170731fda3c923fb4b4c8
Lily <bro21019@byui.edu>
2024-05-03 14:37:19 -0600

    completed prep task 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        string playloop = "yes";
+        int games = 0;
+
+        while (playloop == "yes")
+        {
+            int magic = randomGenerator.Next(1, 100);
+            string gameloop = "yes";
+
+            while (gameloop == "yes")
+            {
+	            Console.WriteLine("What is your guess? ");
+	            string response = Console.ReadLine();
+                int guess = int.Parse(response);
+	
+	            if (guess > magic)
+	            {
+	                Console.WriteLine("Try guessing lower.");
+	            }	
+
+	            else if (guess < magic)
+	            {
+	                Console.WriteLine("Try guessing higher.");
+	            }
+
+	            else if (guess == magic)
+	            {
+	                Console.WriteLine("You guessed it!");
+	                gameloop = "no";
+	                games += 1;
+	            }
+            }
+
+            Console.WriteLine($"You have played {games} games!");
+            Console.WriteLine("Would you like to play again? yes/no");
+            playloop = Console.ReadLine();
+        }
+
+        Console.WriteLine("Thanks for playing!");
     }
 }
